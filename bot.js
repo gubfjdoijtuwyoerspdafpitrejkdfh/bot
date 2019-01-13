6cebc4ead621505ac49a5b99a402841eb0823d02
Mohamed_Ashraf <46439491+gubfjdoijtuwyoerspdafpitrejkdfh@users.noreply.github.com>
2019-01-13 05:42:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -812,7 +812,33 @@ client.on('message', message => {
 });
 
 
-
+client.on('message', message => {
+    var prefix = "&";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix & 'c') {
+        if (!args[1]) {
+    message.channel.send("**اكتب شي بعد الكوماند**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var c = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: c});
+            });
+        }
+        } else {
+            return;
+        }
+    });
 
 
 
